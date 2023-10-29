d9cf3fd533b0837411c0115ee516ac502b01b3b2
Thamires Santos <106447459+thamiresantos@users.noreply.github.com>
2023-10-29 00:05:27 -0300

    exemplo07
    Lista com 2 dimensões

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,7 @@
+const alunos = ["João", "Leo", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias]
+
+console.log(`O aluno da posição 1 da lista de alunos é: ${listaDeAlunosEMedias[0][1]} 
+A nota desse aluno é ${listaDeAlunosEMedias[1][1]}`);
