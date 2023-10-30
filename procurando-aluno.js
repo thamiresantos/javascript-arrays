ca0a86cd5b79d22ba87420d733e3943f05a5808e
Thamires Santos <106447459+thamiresantos@users.noreply.github.com>
2023-10-29 23:03:12 -0300

    exemplo08

---
+++ procurando-aluno.js
@@ -0,0 +1,18 @@
+const alunos = ["João", "Leo", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+function exibeNomeENota(aluno) {
+    if (listaDeAlunosEMedias[0].includes(aluno)) {
+        const indice = listaDeAlunosEMedias[0].indexOf(aluno);
+
+        const mediaDoAluno = listaDeAlunosEMedias[1][indice];
+
+        console.log(`${aluno} tem a media ${mediaDoAluno}`);
+    } else {
+        console.log("Aluno não encontrado!");
+    }
+}
+
+exibeNomeENota("Ana");
