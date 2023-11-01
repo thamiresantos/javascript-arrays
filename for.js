2008f8eb276e85e7b7e1f428c95f68828a566423
Thamires Santos <106447459+thamiresantos@users.noreply.github.com>
2023-10-31 22:21:31 -0300

    For clássico

---
+++ for.js
@@ -0,0 +1,9 @@
+const numeros = [100, 200, 300, 400, 500, 600];
+
+// Primeir expressão: é executada apenas uma única vez
+// Segunda expressão: condição de execução
+// Terceira expressão: é executada sempre ao afinal do bloco
+
+for ( let indice = 0; indice < 6; indice = indice++) {
+    console.log("Oi")
+}
